e87f481b1dc8a36a7bcdf6754c3b67cbb9de96a1
hasegawa-101 <hasegawa@101de-sign.com>
2024-12-06 19:25:45 +0900

    import React を追加

--- app/todos/_containers/dog-list/presentational.tsx
+++ app/todos/_containers/dog-list/presentational.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export function TodoPagePresentation({ todo }: { todo: any }) {
     return (
       <>
-        <h1>{todo.todo}</h1>
+        <h1>{todo.title}</h1>
         <pre>
           <code>{JSON.stringify(todo, null, 2)}</code>
         </pre>
